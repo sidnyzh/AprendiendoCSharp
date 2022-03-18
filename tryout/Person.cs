454c3173816b3944677d15b99a6938b6f4e647f9
Juan Camilo <juanus196@gmail.com>
2022-03-18 16:23:41 -0500

    first commit

---
+++ tryout/Person.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tryout
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public DateTime Birthdate { get; set; }
+        public char Sex { get; set; }
+
+         
+        public Person(string name, DateTime birthdate, char sex)
+        {
+            Name = name;
+            Birthdate = birthdate;
+            Age = accurateAge(birthdate);
+            Sex = sex;
+        }
+
+        public Person()
+        {
+        }
+        private int accurateAge(DateTime DateOfBirth)
+        {
+            int year = DateTime.Now.Year - DateOfBirth.Year;
+            int month = DateTime.Now.Month - DateOfBirth.Month;
+            int day = DateTime.Now.Day - DateOfBirth.Day;
+            if (month < 0)
+            {
+                return year - 1;
+            }
+            else if (month == 0)
+            {
+                return day <=0 ? year - 1: year;
+            }
+            else
+            {
+                return year;
+            }
+        }
+        public List<Person> OverAge(List<Person> people)
+        {
+            List<Person> overAgePeople = people.Where(person => person.Age >=18).ToList();
+            return overAgePeople;
+        }
+        public List<Person> UnderAge(List<Person> people)
+        {
+            List<Person> underAgePeople = people.Where(person => person.Age < 18).ToList();
+            return underAgePeople;
+        }
+        public List<Person> Female(List<Person> people)
+        {
+            List<Person> femalePeople = people.Where(person  => person.Sex == 'F').ToList();
+            return femalePeople;
+        }
+        public List<Person> Male(List<Person> people)
+        {
+            List<Person> malePeople = people.Where(person => person.Sex == 'F' ).ToList();
+            return malePeople;
+        }
+
+        public List<Person> OverAgeFemale(List<Person> people)
+        {
+            List<Person> overAgeFemales = people.Where(person => person.Age >= 18 && person.Sex == 'F').ToList();
+            return overAgeFemales;
+        }
+        public bool BelongsToTheList(string name, List<Person> people)
+        {
+            bool belongs = people.Select(person => person.Name == name).Any();
+            return belongs;
+        }
+    }   
+
+}
---
+++ tryout/Program.cs
@@ -0,0 +1,55 @@
+﻿// See https://aka.ms/new-console-template for more information
+using tryout;
+
+Console.WriteLine("Hello, World!");
+
+List<Person> people = new List<Person>();
+people.Add(new Person("Juan", new DateTime(1996, 02, 04), 'M')); 
+people.Add(new Person("Camilo", new DateTime(1999, 10, 04),'M')); 
+people.Add(new Person("Sidny", new DateTime(1993, 12, 10), 'F')); 
+people.Add(new Person("Julian", new DateTime(1978, 06, 01),'M')); 
+people.Add(new Person("Julio", new DateTime(1987, 03, 20),'M')); 
+people.Add(new Person("Dary", new DateTime(2000, 01, 12), 'F')); 
+people.Add(new Person("Yessica", new DateTime(1986, 10, 22),'F')); 
+people.Add(new Person("Haudy", new DateTime(1995, 02, 19),'M')); 
+people.Add(new Person("steisy", new DateTime(2001, 12, 08),'F')); 
+people.Add(new Person("Camila", new DateTime(2002, 09, 30),'F')); 
+people.Add(new Person("Duván", new DateTime(1996, 02, 04),'M')); 
+people.Add(new Person("Salem", new DateTime(2021, 01, 14),'M')); 
+people.Add(new Person("Bigotes", new DateTime(2021, 10, 26),'M')); 
+people.Add(new Person("Brisa", new DateTime(2022, 02, 04),'F')); 
+people.Add(new Person("Neil", new DateTime(1996, 03, 24),'M')); 
+people.Add(new Person("Raven", new DateTime(1980, 08, 07),'F')); 
+people.Add(new Person("Rue", new DateTime(1983, 02, 18),'F')); 
+people.Add(new Person("Cassie", new DateTime(1990, 11, 27),'F')); 
+people.Add(new Person("Maddie", new DateTime(1975, 03, 15),'F')); 
+people.Add(new Person("Bellamy", new DateTime(1992, 08, 11),'M')); 
+people.Add(new Person("Eliza", new DateTime(2005, 12, 12),'F')); 
+people.Add(new Person("Carol", new DateTime(1986, 07, 18),'F')); 
+people.Add(new Person("Lexie", new DateTime(2001, 06, 02),'F')); 
+people.Add(new Person("Fezco", new DateTime(1998, 05, 07),'M')); 
+people.Add(new Person("Neil", new DateTime(1974, 12, 29),'M')); 
+people.Add(new Person("Fraileón", new DateTime(1920, 08, 30),'M')); 
+people.Add(new Person("Wendy", new DateTime(2000, 11, 07),'F')); 
+people.Add(new Person("Valentina", new DateTime(1999, 11, 23),'F')); 
+people.Add(new Person("Dayana", new DateTime(1995, 05, 16),'F')); 
+people.Add(new Person("Melissa", new DateTime(2019, 10, 25),'F')); 
+people.Add(new Person("Natalia", new DateTime(1994, 11, 02),'F')); 
+people.Add(new Person("Dahiana", new DateTime(1991, 07, 06),'F'));
+
+Person person = new Person();
+
+
+
+
+List<Person> overAges=  person.OverAge(people);
+List<Person> underAges = person.UnderAge(people);
+List<Person> females = person.Female(people);
+List<Person> males = person.Male(people);
+List<Person> overAgeFemales = person.OverAgeFemale(people);
+Console.WriteLine("Aoy una crack");
+
+
+
+
+
